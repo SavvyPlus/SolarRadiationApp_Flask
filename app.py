52bac35cbe3e8b342b67d9d97a95da8bbd1b3be8
ykydxT <ykydxT@outlook.com>
2018-08-13 15:30:37 +1000

    with first template

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/test')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/')
+def index():
+    posts = [{'a': 'aaaa'}, {'a': 'bb'}]
+
+    return render_template('index.html', title='awsom', posts=posts)
 
 
 if __name__ == '__main__':
